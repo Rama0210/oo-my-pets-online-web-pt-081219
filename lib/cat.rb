616ee1d64494f0c3492025eccd3f3b42957cbb2e
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-09-26 23:18:34 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,13 +1,13 @@
-def cats
-   Cat.all.select do |cat|
-     cat.owner == self
-   end
- end
- def dogs
-   Dog.all.select do |dog|
-     dog.owner == self
-   end
- end
- def buy_cat(name)
-   Cat.new(name, self)
- end
+# def cats
+#   Cat.all.select do |cat|
+#     cat.owner == self
+#   end
+# end
+# def dogs
+#   Dog.all.select do |dog|
+#     dog.owner == self
+#   end
+# end
+# def buy_cat(name)
+#   Cat.new(name, self)
+# end
