be2880f1012ee704d01d85b3f85e457b9d3a6e13
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-09-26 23:37:42 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -6,15 +6,5 @@ class Cat
     @name = name
     @mood = "nervous"
   end
-
-
-def cats
-  Cat.all.select do |cat|
-    cat.owner == self
-  end
-  
-
-def buy_cat(name)
-  Cat.new(name, self)
 end
 
