0ef8381bd5ac6dba6b49c626b51665ff562689a6
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-09-26 23:05:51 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,20 @@
 class Owner
-  # code goes here
-end
+ attr_accessor :say_species, :mood, :dog
+ attr_reader :name, :species
+@@all = []
+ def initialize(name)
+   @name = name
+   @species = "human"
+   @say_species = "I am a human."
+   @@all << self
+   @mood = "nervous"
+ end
+ def self.all
+   @@all
+ end
+ def self.count
+   all.count
+ end
+ def self.reset_all
+   all.clear
+ end
