02b97aa7beb6066664d5f7033ecbd268274cbaa9
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-09-26 23:07:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
-class Dog
-  # code goes here
-end
+def buy_dog(name)
+   Dog.new(name,self)
+ end
+ def walk_dogs
+  Dog.all.select do |dog|
+   dog.mood = "happy"
+ end
+ end
+ def feed_cats
+   Cat.all.select do |cat|
+     cat.mood = "happy"
+   end
+ end
+ def sell_pets
+   Dog.all.select do |dog|
+   dog.mood = "nervous"
+   dog.owner = nil
+ end
