ea8ee707b9fd99e07567899fa9f694d9000adcea
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-09-26 23:06:28 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,13 @@
-class Cat
-  # code goes here
-end
+def cats
+   Cat.all.select do |cat|
+     cat.owner == self
+   end
+ end
+ def dogs
+   Dog.all.select do |dog|
+     dog.owner == self
+   end
+ end
+ def buy_cat(name)
+   Cat.new(name, self)
+ end
