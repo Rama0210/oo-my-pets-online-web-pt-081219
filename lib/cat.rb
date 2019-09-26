f8e3eabf12ed64fb5033aaa2e1a2c136de164b42
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-09-26 23:34:56 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,23 +1,23 @@
-# def cats
-#   Cat.all.select do |cat|
-#     cat.owner == self
-#   end
-# end
-# def dogs
-#   Dog.all.select do |dog|
-#     dog.owner == self
-#   end
-# end
-# def buy_cat(name)
-#   Cat.new(name, self)
-# end
-
-class Cat
-  attr_reader :name
-   attr_accessor :mood
-  
-  def initialize(name)
-    @name = name
-    @mood = "nervous"
+def cats
+  Cat.all.select do |cat|
+    cat.owner == self
   end
 end
+def dogs
+  Dog.all.select do |dog|
+    dog.owner == self
+  end
+end
+def buy_cat(name)
+  Cat.new(name, self)
+end
+
+# class Cat
+#   attr_reader :name
+#   attr_accessor :mood
+  
+#   def initialize(name)
+#     @name = name
+#     @mood = "nervous"
+#   end
+# end
